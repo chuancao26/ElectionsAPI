6cfb65a477c2c0475d55ef34002bdaa2b149aaca
chuancao26 <chuancao@unsa.edu.pe>
2025-07-11 20:35:10 -0500

    adding swagger to the filter

--- src/main/java/com/Elecciones/elections/config/SecurityConfig.java
+++ src/main/java/com/Elecciones/elections/config/SecurityConfig.java
@@ -20,10 +20,16 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/google").permitAll()
+                        .requestMatchers(
+                                "/api/auth/google",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter,
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
